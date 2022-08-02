f1bc6b9b835d5837a3d787ccb9ce6711279b6c82
blanec12 <blanecummings@hotmail.com>
2022-08-01 20:42:08 -0500

    Init about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,13 @@ import React from "react";
 export default function About() { 
     return (
         <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <h1>Hi, I'm Blane.
+                    <br/>I like building cool stuff.
+                </h1>
+                
+                
+            </div>
         </section>
     );
 }
