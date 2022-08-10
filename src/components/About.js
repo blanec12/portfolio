598b537f3249912720baafda0d501919f9a6abee
blanec12 <blanecummings@hotmail.com>
2022-08-09 21:31:17 -0500

    add avatar image to about

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
-import React from "react";
+
 
 export default function About() { 
     return (
         <section id="about">
-            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                <h1>Hi, I'm Blane.
-                    <br/>I like building cool stuff.
-                </h1>
+            <div className="container mx-auto px-10 py-20 flex flex-col items-center justify-center">
+            <img className="rounded-full w-80 h-80" alt="avatar" src="https://avatars.githubusercontent.com/u/44101795?v=4"/>
+                <div className="container px-5 py-10 mx-auto text-center lg:px-40">
                 
-                
+                    <h1 className="title-font font-medium text-white text-3xl text-center mb-4">Hi, I'm Blane.</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-center mb-10">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Id faucibus nisl tincidunt eget nullam non nisi. Eu lobortis elementum 
+                    nibh tellus molestie nunc non blandit massa. Mauris sit amet massa vitae. Ac turpis egestas sed tempus. 
+                    Arcu risus quis varius quam quisque. Elementum tempus egestas sed sed risus. Fermentum iaculis eu non 
+                    diam phasellus vestibulum. Morbi tristique senectus et netus et. Amet venenatis urna cursus eget. 
+                    Blandit libero volutpat sed cras ornare arcu.
+                    </p>
+                </div>
             </div>
         </section>
     );
