14f2414a83ee602cb3f67f8269db65c013dc2456
Blane Cummings <blanecummings@hotmail.com>
2023-08-03 17:42:28 -0500

    redo most everything

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,72 +1,47 @@
-import { Link } from "react-scroll";
-
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-dark navbar-expand-md bg-dark py-3 sticky-top">
+    <nav className="navbar navbar-expand-md bg-dark py-3" data-bs-theme="dark">
       <div className="container">
-        <a className="navbar-brand d-flex align-items-center" href="#">
-          <span style={{ fontSize: 30, fontWeight: "bold" }}>
-            Blane Cummings
-          </span>
+        <a
+          className="navbar-brand d-flex align-items-center h1 fs-2"
+          href="#banner"
+        >
+          <span>./Blane Cummings</span>
         </a>
         <button
-          className="navbar-toggler"
           data-bs-toggle="collapse"
+          className="navbar-toggler"
           data-bs-target="#navcol-5"
         >
           <span className="visually-hidden">Toggle navigation</span>
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div id="navcol-5" className="collapse navbar-collapse">
+        <div className="collapse navbar-collapse" id="navcol-5">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link
-                className="nav-link"
-                href=""
-                style={{ fontSize: 20 }}
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-100}
-              >
-                About
-              </Link>
+              <a className="nav-link fs-5" href="#skills">
+                My skills
+              </a>
             </li>
             <li className="nav-item">
-              <Link
-                className="nav-link"
-                href=""
-                style={{ fontSize: 20 }}
-                to="projects"
-                spy={true}
-                smooth={true}
-                offset={-100}
-              >
+              <a className="nav-link fs-5" href="#projects">
                 Projects
-              </Link>
+              </a>
             </li>
             <li className="nav-item">
-              <Link
-                className="nav-link"
-                href=""
-                style={{ fontSize: 20 }}
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-100}
+              <a className="nav-link fs-5" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link fs-5"
+                href="mailto:blanecummings@hotmail.com"
               >
                 Contact
-              </Link>
+              </a>
             </li>
           </ul>
-          <a
-            className="btn btn-primary ms-md-2"
-            style={{ fontSize: 20 }}
-            role="button"
-            href="https://myblog.com"
-          >
-            Blog
-          </a>
         </div>
       </div>
     </nav>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,85 +3,12 @@ export default function Projects() {
     <div id="projects" className="container py-4 py-xl-5">
       <div className="row mb-5">
         <div className="col-md-8 col-xl-6 text-center mx-auto">
-          <h2 style={{ fontSize: 40, fontWeight: "bold" }}>Projects </h2>
-          <i
-            className="fas fa-code"
-            style={{ fontSize: 35, marginBottom: 15 }}
-          ></i>
-          <p className="w-lg-50" style={{ fontSize: 20 }}>
-            Curae hendrerit donec commodo hendrerit egestas tempus, turpis
-            facilisis nostra nunc. Vestibulum dui eget ultrices.
+          <h2 className="fw-bold">Projects</h2>
+          <p className="w-lg-50 fs-5 mb-5">
+            I am currently working on some projects to populate this section. It
+            is hard to complete projects when you already work a full-time job
+            :D
           </p>
-        </div>
-      </div>
-      <div className="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3">
-        <div className="col">
-          <div>
-            <img
-              className="rounded img-fluid d-block w-100 fit-cover"
-              style={{ height: 200 }}
-              src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
-            />
-            <div className="py-4">
-              <h4>Sorting Algorithm Visualizer</h4>
-              <p>
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
-                elit non mi porta gravida at eget metus.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div>
-            <img
-              className="rounded img-fluid d-block w-100 fit-cover"
-              style={{ height: 200 }}
-              src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
-            />
-            <div className="py-4">
-              <h4>Web Chat Client</h4>
-              <p>
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
-                elit non mi porta gravida at eget metus.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div>
-            <img
-              className="rounded img-fluid d-block w-100 fit-cover"
-              style={{ height: 200 }}
-              src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
-            />
-            <div className="py-4">
-              <h4>Pathfinding Algorithm Visualizer</h4>
-              <p>
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
-                elit non mi porta gravida at eget metus.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div>
-            <img
-              className="rounded img-fluid d-block w-100 fit-cover"
-              style={{ height: 200 }}
-              src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
-            />
-            <div className="py-4">
-              <h4>Some other project here....</h4>
-              <p>
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
-                elit non mi porta gravida at eget metus.
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
